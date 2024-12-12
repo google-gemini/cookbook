ec02f2a751d7252bfaded41d87118ec757533315
Mark Daoust <markdaoust@google.com>
2024-12-12 07:38:35 -0800

    use .text and .data

--- gemini-2/live_api_starter.py
+++ gemini-2/live_api_starter.py
@@ -28,6 +28,7 @@ from google import genai
 
 if sys.version_info < (3, 11, 0):
     import taskgroup, exceptiongroup
+
     asyncio.TaskGroup = taskgroup.TaskGroup
     asyncio.ExceptionGroup = exceptiongroup.ExceptionGroup
 
@@ -39,11 +40,9 @@ CHUNK_SIZE = 512
 
 MODEL = "models/gemini-2.0-flash-exp"
 
-client = genai.Client(
-    http_options={'api_version': 'v1alpha'})
+client = genai.Client(http_options={"api_version": "v1alpha"})
 
-CONFIG={
-    "generation_config": {"response_modalities": ["AUDIO"]}}
+CONFIG = {"generation_config": {"response_modalities": ["AUDIO"]}}
 
 pya = pyaudio.PyAudio()
 
@@ -51,14 +50,9 @@ pya = pyaudio.PyAudio()
 class AudioLoop:
     def __init__(self):
         self.audio_in_queue = asyncio.Queue()
-        self.audio_out_queue = asyncio.Queue()
-        self.video_out_queue = asyncio.Queue()
+        self.out_queue = asyncio.Queue()
 
         self.session = None
-
-        self.send_text_task = None
-        self.receive_audio_task = None
-        self.play_audio_task = None
 
     async def send_text(self):
         while True:
@@ -88,7 +82,9 @@ class AudioLoop:
     async def get_frames(self):
         # This takes about a second, and will block the whole program
         # causing the audio pipeline to overflow if you don't to_thread it.
-        cap = await asyncio.to_thread(cv2.VideoCapture, 0)  # 0 represents the default camera
+        cap = await asyncio.to_thread(
+            cv2.VideoCapture, 0
+        )  # 0 represents the default camera
 
         while True:
             frame = await asyncio.to_thread(self._get_frame, cap)
@@ -97,14 +93,14 @@ class AudioLoop:
 
             await asyncio.sleep(1.0)
 
-            self.video_out_queue.put_nowait(frame)
+            self.out_queue.put_nowait(frame)
 
         # Release the VideoCapture object
         cap.release()
 
-    async def send_frames(self):
+    async def send_realtime(self):
         while True:
-            frame = await self.video_out_queue.get()
+            frame = await self.out_queue.get()
             await self.session.send(frame)
 
     async def listen_audio(self):
@@ -122,44 +118,34 @@ class AudioLoop:
         )
         while True:
             data = await asyncio.to_thread(stream.read, CHUNK_SIZE)
-            self.audio_out_queue.put_nowait(data)
-
-    async def send_audio(self):
-        while True:
-            chunk = await self.audio_out_queue.get()
-            await self.session.send({"data": chunk, "mime_type": "audio/pcm"})
+            self.out_queue.put_nowait({"data": data, "mime_type": "audio/pcm"})
 
     async def receive_audio(self):
         "Background task to reads from the websocket and write pcm chunks to the output queue"
         while True:
-            async for response in self.session.receive():
-                server_content = response.server_content
-                if server_content is not None:
-                    model_turn = server_content.model_turn
-                    if model_turn is not None:
-                        parts = model_turn.parts
+            turn = self.session.receive()
+            async for response in turn:
+                if data := response.data:
+                    self.audio_in_queue.put_nowait(data)
+                    continue
+                if text := response.text:
+                    print(text, end="")
 
-                        for part in parts:
-                            if part.text is not None:
-                                print(part.text, end="")
-                            elif part.inline_data is not None:
-                                self.audio_in_queue.put_nowait(part.inline_data.data)
-
-                    server_content.model_turn = None
-                    turn_complete = server_content.turn_complete
-                    if turn_complete:
-                        # If you interrupt the model, it sends a turn_complete.
-                        # For interruptions to work, we need to stop playback.
-                        # So empty out the audio queue because it may have loaded
-                        # much more audio than has played yet.
-                        print("Turn complete")
-                        while not self.audio_in_queue.empty():
-                            self.audio_in_queue.get_nowait()
+            # If you interrupt the model, it sends a turn_complete.
+            # For interruptions to work, we need to stop playback.
+            # So empty out the audio queue because it may have loaded
+            # much more audio than has played yet.
+            while not self.audio_in_queue.empty():
+                self.audio_in_queue.get_nowait()
 
     async def play_audio(self):
         pya = pyaudio.PyAudio()
         stream = await asyncio.to_thread(
-            pya.open, format=FORMAT, channels=CHANNELS, rate=RECEIVE_SAMPLE_RATE, output=True
+            pya.open,
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RECEIVE_SAMPLE_RATE,
+            output=True,
         )
         while True:
             bytestream = await self.audio_in_queue.get()
@@ -184,10 +170,9 @@ class AudioLoop:
 
             send_text_task.add_done_callback(cleanup)
 
+            tg.create_task(self.send_realtime())
             tg.create_task(self.listen_audio())
-            tg.create_task(self.send_audio())
             tg.create_task(self.get_frames())
-            tg.create_task(self.send_frames())
             tg.create_task(self.receive_audio())
             tg.create_task(self.play_audio())
 

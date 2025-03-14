121ebdac406848ff13bbefbd3ee2bfbd4407fb7d
Freddy Boulton <alfonsoboulton@gmail.com>
2025-03-13 17:04:32 -0700

    Fix

--- examples/fastrtc_ui.py
+++ examples/fastrtc_ui.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Freddy Boulton (https://github.com/freddyaboulton)
+# Copyright 2025 Google LLC
 #
+# Written by:
+#   - Freddy Boulton (https://github.com/freddyaboulton)
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at

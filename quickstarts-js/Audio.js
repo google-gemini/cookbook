b753cd9340a8196896f72dfa96dceb13dda1d01c
Giom-V <guillaume.vernade__github@normalesup.org>
2025-12-11 16:09:31 +0100

    Adding fiercefalcon to the model dropdown

--- quickstarts-js/Audio.js
+++ quickstarts-js/Audio.js
@@ -53,7 +53,7 @@ For more information about all Gemini models, check the [documentation](https://
 */
 
 // [CODE STARTS]
-MODEL_ID="gemini-2.5-flash" // "gemini-2.5-flash", "gemini-2.5-pro", "gemini-2.0-flash", "gemini-2.5-flash-lite-preview-06-17
+MODEL_ID = "gemini-2.5-flash" // "gemini-2.5-flash-lite", "gemini-2.5-flash""gemini-2.5-pro", "fiercefalcon", "gemini-3-pro-preview"
 // [CODE ENDS]
 
 /* Markdown (render)
